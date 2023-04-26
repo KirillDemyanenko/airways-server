56944fff6e62f64da309947a98c8273ca76df2cf
KirillDemyanenko <demyanenkokg@gmail.com>
2023-04-26 20:54:27 +0300

    feature/auth - create controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+
+export class CreateUser {
+  username: string;
+  password: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAll(): any {
+    return this.appService.getAllUsers();
+  }
+
+  @Post('validate')
+  async postValidate(@Body() login: { login: string }) {
+    return (await this.appService.validateLogin(login.login))
+      ? { status: true, message: `login: «${login.login}» is available` }
+      : { status: false, message: `login: «${login.login}» already exist` };
+  }
+
+  @Post('registration')
+  async postUser(@Body() user: CreateUser) {
+    return (await this.appService.registration(user))
+      ? {
+          status: true,
+          message: `successfully registered new user «${user.username}»`,
+        }
+      : { status: false, message: `registration failed` };
+  }
+
+  @Post('login')
+  async postLogin(@Body() user: CreateUser) {
+    return await this.appService.login(user);
   }
 }
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '`HmCE-(CkaX<:*`3P@vm+Sx)bw]8~[Ee+ab:@1xK?NOqGoTZvypaxII&I:H*bq@',
+};
