fee186349dfea34bddb384dd2c85afe1689a2277
KirillDemyanenko <demyanenkokg@gmail.com>
2023-04-27 13:19:18 +0300

    feat/app - add airports to database

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,24 @@
 export const jwtConstants = {
   secret: '`HmCE-(CkaX<:*`3P@vm+Sx)bw]8~[Ee+ab:@1xK?NOqGoTZvypaxII&I:H*bq@',
 };
+
+export enum TypeOfFlight {
+  oneWay = 'one-way',
+  roundTrip = 'round-trip',
+}
+
+/* to add airports to the database you need to add them below */
+export const air = {
+  YNHL: {
+    icao: 'YNHL',
+    iata: '',
+    name: 'Nhill Airport',
+    city: '',
+    state: 'Victoria',
+    country: 'AU',
+    elevation: 454,
+    lat: -36.3097000122,
+    lon: 141.6410064697,
+    tz: 'Australia/Melbourne',
+  },
+};
---
+++ src/entitys/flying.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import { TypeOfFlight } from '../constants';
+
+@Entity()
+export class Flying extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: TypeOfFlight;
+
+  @Column()
+  destinationFrom: string;
+
+  @Column()
+  destinationTo: string;
+
+  @Column()
+  departureDate: Date;
+
+  @Column()
+  returnDate?: Date | null;
+
+  @Column()
+  costUSD: number;
+}
